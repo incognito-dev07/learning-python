f6aeafd154d5ad3fc507936e4d9d70fd0f8bb803
incognito-dev07 <osasanolusola2007@gmail.com>
2026-05-23 12:23:22 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is my first python program
+
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+
+print(f"My name is {name}")
+print(f"I am currently {age} years old\n")
+
+age+=1
+print("Happy birthday to Me")
+print(f"{name} is now {age} years old")
