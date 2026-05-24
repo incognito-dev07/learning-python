acf9540745767d6e855400b2f9c27c5f7aa97048
incognito-dev07 <osasanolusola2007@gmail.com>
2026-05-24 09:13:26 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is my first python program
+''' This is my first python program
 
 name = input("What is your name: ")
 age = int(input("What is your age: "))
@@ -9,3 +9,14 @@ print(f"I am currently {age} years old\n")
 age+=1
 print("Happy birthday to Me")
 print(f"{name} is now {age} years old")
+'''
+
+x = 3.14
+y = -4
+z = 5
+
+print(round(x))
+print(abs(y))
+print(pow(z, 2))
+print(max(x,y,z))
+print(min(x,y,z))
