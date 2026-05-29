372d96e5d1aeb3e18bb80a30f0dbf82d7e479d9e
incognito-dev07 <osasanolusola2007@gmail.com>
2026-05-29 11:42:05 +0100

    minor changes

--- string.py
+++ string.py
@@ -7,3 +7,14 @@ print(step_slicing)
 
 reverse_second_word = second_word[::-1]
 print(reverse_second_word)
+
+# string methods
+text = "Hello world"
+text.upper() # converts to upper case
+text.lower() # converts to lower case
+text.strip() # removes whitespaces
+text.replace(old, new) # replaces old with new
+text.split(seperator) # splits into list
+
+
+print(text.split(" "))
