9b6d924abb58953e6a58d4b9d533dc4ed5ca3981
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-01 18:10:01 +0100

    minor changes

---
+++ game_character.py
@@ -0,0 +1,23 @@
+full_dot = '●'
+empty_dot = '○'
+
+def create_character(name, strength, intelligence, charisma):
+  if not isinstance(name, str):
+    return "The character name should be a string"
+  if not name:
+    return "The character should have a name"
+  if len(name) > 10:
+    return "The character name is too long"
+  if name.find(" ") != -1:
+    return "The character name should not contain spaces"
+  if not (isinstance(strength, int) and isinstance(intelligence, int) and isinstance(charisma, int)):
+    return "All stats should be integers"
+  if strength < 1 or intelligence < 1 or charisma < 1:
+    return "All stats should be no less than 1"
+  if strength > 4 or intelligence > 4 or charisma > 4:
+    return "All stats should be no more than 4"
+  if (strength + intelligence + charisma) != 7:
+    return "The character should start with 7 points"
+  return f"Name: {name}\nSTR: {full_dot * strength}{empty_dots * (10 - strength)}\nINT: {full_dot * intelligence}{empty_dots * (10 - intelligence)}\nCHA: {full_dot * charisma}{empty_dot * (10 - charisma)}"
+  
+print(create_character("Nigga", 4, 2, 1))
