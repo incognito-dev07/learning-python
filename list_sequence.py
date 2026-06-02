edbf472da2d48e8e4c0fd7f8292245c1a491e003
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-02 23:21:37 +0100

    minor fixes

--- list_sequence.py
+++ list_sequence.py
@@ -18,6 +18,7 @@ print(rest) # prints a list of the remainder
 
 
 numbers = [1, 2, 3, 4, 5]
-numbers.append(6) # adds to end of the list
+remaining = [6, 7, 8]
+numbers.append(remaining) # adds to end of the list
 
 print(numbers)
