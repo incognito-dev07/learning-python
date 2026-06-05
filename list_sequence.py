2e6473ac3ced6a2eb50bd9111a584c1234825fbf
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-05 09:40:12 +0100

    minor fixes

--- list_sequence.py
+++ list_sequence.py
@@ -57,3 +57,5 @@ print(new)
 # NB: List and Tuple slicing works the same way as string slicing
 
 # The sorted() method in lists and tuples can have optional arguments like 'reverse' and 'key'
+languages = ["dart", "python", "rust", "java", "c++", "javascript", "lua", "assembly", "swift", "kotlin"]
+sorted(lang)
