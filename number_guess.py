1db818e4bc78526200674e804eb333420f5360c2
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-06 11:10:25 +0100

    minor fixes

--- number_guess.py
+++ number_guess.py
@@ -4,5 +4,15 @@ answer = random.randint(0, 100)
 attempts = 0
 
 while attempts < 5:
-  user_input = input("Guess the number: ")
-  break
+  user_input = input("Guess the number (1-100): ")
+  if user_input == answer:
+    print(f"Correct: The result is {user_input}")
+    break
+  else:
+    attempts += 1
+    if user_input > answer:
+      print("Too high. Try again")
+    elif user_input < answer:
+      print("Too low. Try again")
+    print("{5 - attempts} attempts left)\n")
+  
