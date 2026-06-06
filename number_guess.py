53df6777f5156a84d4eb6285d41ed2c818d06149
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-06 11:11:13 +0100

    minor fixes

--- number_guess.py
+++ number_guess.py
@@ -4,7 +4,7 @@ answer = random.randint(0, 100)
 attempts = 0
 
 while attempts < 5:
-  user_input = input("Guess the number (1-100): ")
+  user_input = int(input("Guess the number (1 - 100): "))
   if user_input == answer:
     print(f"Correct: The result is {user_input}")
     break
