e38ac88f15d5bf2c30dca59c4a2cd715164be3da
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-12 17:48:24 +0100

    minor fixes

--- number_pattern.py
+++ number_pattern.py
@@ -2,7 +2,7 @@ def number_pattern(n):
   if not isinstance(n, int):
     return "Argument must be an integer value."
   elif n < 1:
-    return "Argument must be an integer greater than 0"
+    return "Argument must be an integer greater than 0."
   else:
     result = []
     for i in range(n):
