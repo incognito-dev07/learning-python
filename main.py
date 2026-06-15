09c3ba849ebe5815a9c9660c2a1e14d38e6c5198
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-15 20:10:38 +0100

    minor fixes

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-# Temperature Conversion Program
-unit = input("Is the temperature in Celsius or Fahrenheit (C/F): ")
-temp = float(input("Enter the temperature: "))
-
-if unit == "C":
-  temp = round((9 * temp) / 5 + 32, 1)
-  print(f"The temperature in Fahrenheit is: {temp}°F")
-elif unit == "F":
-  temp = round((temp - 32) * 5 / 9, 1)
-  print(f"The temperature in celsius is: {temp}°C")
-else:
-  print(f"{unit} is an invalid temperature unit")
-  
-  
-  
-number = 18
-message = "even number" if number % 2 == 0 else "odd number"
-print(f"{number} is an {message}")
+# Ternary Operator
+name = "Incognito"
+age = 119
+print(f"Welcome, {name}") if age >= 18 else print("Access Denied")
