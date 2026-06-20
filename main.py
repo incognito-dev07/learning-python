91f7c2f274bce37d28da1ec431c5654d8b1ca180
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-20 01:05:57 +0100

    minor fixes

--- main.py
+++ main.py
@@ -9,36 +9,33 @@ class RPSGame:
     self.options = ["rock", "paper", "scissors"]
     self.shortcuts = {"r": "rock", "p": "paper", "s": "scissors"}
     self.winning_pairs = [("rock", "scissors"), ("paper", "rock"), ("scissors", "paper")]
+    
+  def get_player_choice(self):
+    while True:
+      choice = input("\nEnter your choice (r/p/s): ").lower()
+      if choice in self.shortcuts:
+        return self.shortcuts[choice]
+      else:
+        print("Invalid Input. Try again!")
 
-  # Game Logic
-  def start_game(self):
-  player = input("Enter your choice (r/p/s): ").lower()
-  player = shortcuts.get(player)
-  bot = random.choice(options)
+  def play_round(self):
+    player = self.get_player_choice()
+    bot = random.choice(self.options)
+    print(f"\nYour Choice: {player.capitalize()} \nBot Choice: {bot.capitalize()}")
+    time.sleep(0.5)
   
-  if player == None:
-    print("Invalid Input. Try again!")
-  elif (player, bot) in winning_pairs:
-    player_wins += 1
-    display_choices(player, bot)
-    print("Congrats. You win!")
-  elif player == bot:
-    display_choices(player, bot)
-    print("It's a Tie Game")
-  else:
-    bot_wins += 1
-    display_choices(player, bot)
-    print("Bot Wins. Try again!")
+    if (player, bot) in self.winning_pairs:
+      self.player_wins += 1
+      print("You win. Congratulations!")
+    elif player == bot:
+      print("It's a Tie Game")
+    else:
+      self.bot_wins += 1
+      print("Bot Win. Try again!")
 
-# Check Score History
   def check_history(self):
     print("\nSCORE HISTORY")
     print(f"Player Wins: {self.player_wins} \nBot Wins: {self.bot_wins}")
-  
-# Utility Functions
-def display_choices(player, bot):
-  print(f"\nYour Choice: {player.capitalize()} \nBot Choice: {bot.capitalize()}")
-  time.sleep(0.5)
  
   # Main Entry Point 
   def run(self):
@@ -51,18 +48,17 @@ def display_choices(player, bot):
   
       if choice == "1":
         while True:
-        play_game()
-        if input("\nPlay again? (y/n): ") != "y":
-          break
+          self.play_round()
+          if input("\nPlay again? (y/n): ").lower() != "y": break
       elif choice == "2":
-        check_history()
+        self.check_history()
       elif choice == "3":
         sys.exit("\nThanks for playing the game")
       else:
         print("Invalid option. Try again!")
         
 if __name__ == "__main__":
-  RPSGame.run()
+  RPSGame().run()
 
 
 
