931b57231b6640e22ec979d63f214d5dac5315dd
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-21 20:48:21 +0100

    minor fixes

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+response = requests.get("https://media-downloader-7ovf.onrender.com/api/ping")
+print(response)
+
+data = response.json()
+print(data)
+
+page = requests.get("https://media-saver-hub.vercel.app")
+soup = BeautifulSoup(page.text, "html.parser")
+print(soup)
+
+
+
+
+
 # Ternary Operator
 name = "Incognito"
 age = 119
