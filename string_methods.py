047ef4e232e769fb369a529dc5cc9bb454ff33c6
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-28 00:29:12 +0100

    minor changes

--- string_methods.py
+++ string_methods.py
@@ -1,37 +1,39 @@
 # String Functions
-name = "Incognito"
-len(name) # returns the string length
+text = "Incognito is Peak"
+len(text) # returns the total string length
+
+print(text.count(""))
 
 
-# String Methods
-text = "Hello world"
-text.upper() # converts string to upper case
-text.lower() # converts string to lower case
-text.replace(old, new) # replaces old with new
-text.split(sign) # splits into list
-text.startswith(pre) # checks the first char
-text.endswith(suff) # checks if it's last char
-text.find(char) # returns idx of first occurence
-text.count(char) # counts total char occurence
-text.isupper() # checks if all char is uppercase
-text.islower() # checks if all char is lowercase
-text.capitalize() # makes first letter uppercase
-text.title() # makes first char of each word upper
-text.strip() # removes whitespaces
-# lstrip() and rstrip() removes spaces from left and right respectively
+# # String Methods
+# text = "Hello world"
+# text.upper() # converts string to upper case
+# text.lower() # converts string to lower case
+# text.replace(old, new) # replaces old with new
+# text.split(sign) # splits into list
+# text.startswith(pre) # checks the first char
+# text.endswith(suff) # checks if it's last char
+# text.find("") # returns idx of first occurence
+# text.count("") # returns how often a word appears
+# text.isupper() # checks if all char is uppercase
+# text.islower() # checks if all char is lowercase
+# text.capitalize() # makes first letter uppercase
+# text.title() # makes first char of each word upper
+# text.strip() # removes whitespaces
+# # lstrip() and rstrip() removes spaces from left and right respectively
 
-print(text.capitalize())
-
-
+# print(text.capitalize())
 
 
 
-# string slicing
-greeting = "Hello world"
 
-second_word = greeting[6:]
-step_slicing = greeting[0:5:2]
-print(step_slicing)
 
-reverse_second_word = second_word[::-1]
-print(reverse_second_word)
+# # string slicing
+# greeting = "Hello world"
+
+# second_word = greeting[6:]
+# step_slicing = greeting[0:5:2]
+# print(step_slicing)
+
+# reverse_second_word = second_word[::-1]
+# print(reverse_second_word)
