bc127222b5e875beaef99962084bf072c8aea296
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-28 00:17:24 +0100

    minor changes

--- string_methods.py
+++ string_methods.py
@@ -1,28 +1,37 @@
-greeting = "Hello world"
+# String Functions
+name = "Incognito"
+len(name) # returns the string length
+
+
+# String Methods
+text = "Hello world"
+text.upper() # converts string to upper case
+text.lower() # converts string to lower case
+text.replace(old, new) # replaces old with new
+text.split(sign) # splits into list
+text.startswith(pre) # checks the first char
+text.endswith(suff) # checks if it's last char
+text.find(char) # returns idx of first occurence
+text.count(char) # counts total char occurence
+text.isupper() # checks if all char is uppercase
+text.islower() # checks if all char is lowercase
+text.capitalize() # makes first letter uppercase
+text.title() # makes first char of each word upper
+text.strip() # removes whitespaces
+# lstrip() and rstrip() removes spaces from left and right respectively
+
+print(text.capitalize())
+
+
+
+
 
 # string slicing
+greeting = "Hello world"
+
 second_word = greeting[6:]
 step_slicing = greeting[0:5:2]
 print(step_slicing)
 
 reverse_second_word = second_word[::-1]
 print(reverse_second_word)
-
-# string methods
-text = "Hello world"
-text.upper() # converts to upper case
-text.lower() # converts to lower case
-text.replace(old, new) # replaces old with new
-text.split(seperator) # splits into list
-text.startswith(prefix) 
-text.endswith(suffix) # returns boolean
-text.find(char) # returns idx of first occurence
-text.count(char) # counts total char occurence
-text.isupper() # checks if all char is uppercase
-text.islower() # checks if all char is lowercase
-text.capitalize() # makes first letter uppercase
-text.title() # makes first letter of each word uppercase
-text.strip() # removes whitespaces
-# lstrip() and rstrip() removes spaces from left and right respectively
-
-print(text.capitalize())
