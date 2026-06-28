dae4bddf37d5b6087da0e13ad7292b4cc6ee1ca4
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-28 16:42:33 +0100

    minor changes

--- practice_projects/num_guess.py
+++ practice_projects/num_guess.py
@@ -2,9 +2,7 @@ import random
 from colorama import Fore, Style
 
 print(f"\n{Fore.GREEN}Welcome to the Number Guessing Game!")
-print("-" * 45)
-print("I have selected a number between 1 and 100.\nTry to guees it!")
-print("-" * 45)
+print("-" * 45, "\nI have selected a number between 1 and 100.\nTry to guees it!\n","-" * 45)
 
 answer = random.randint(1, 101)
 print(answer)
